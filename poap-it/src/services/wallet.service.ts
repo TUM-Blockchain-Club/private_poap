5ed251a8e4869e0de1f18a21f9bd801eac8fd0b3
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-09-17 14:37:50 +0200

    Get MasterWallet from local storage

--- poap-it/src/services/wallet.service.ts
+++ poap-it/src/services/wallet.service.ts
@@ -2,6 +2,9 @@ import { ethers } from 'ethers';
 
 export function InitWallet() {
     let masterWallet = ethers.Wallet.createRandom();
-
     localStorage.setItem('masterWallet', JSON.stringify(masterWallet));
+}
+
+export function getMasterWallet(): ethers.Wallet {
+    return JSON.parse(localStorage.getItem('masterWallet')!)
 }
