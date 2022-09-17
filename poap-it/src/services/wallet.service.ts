568bfdf28488980b27163fe0f82b97697098a5c3
Valentin Hartig <Valentin.Hartig@gmail.com>
2022-09-17 15:37:47 +0200

    Add createPOAP feature

--- poap-it/src/services/wallet.service.ts
+++ poap-it/src/services/wallet.service.ts
@@ -1,4 +1,8 @@
-import { ethers } from 'ethers';
+import axios from 'axios';
+import {ethers} from 'ethers';
+
+const POAP_API_KEY = '';
+const POAP_API_TOKEN = '';
 
 export function InitWallet() {
     let masterWallet = ethers.Wallet.createRandom();
@@ -6,5 +10,71 @@ export function InitWallet() {
 }
 
 export function getMasterWallet(): ethers.Wallet {
-    return JSON.parse(localStorage.getItem('masterWallet')!)
+    let walletString = localStorage.getItem('masterWallet')
+    if (walletString === null || walletString === '') {
+        InitWallet();
+        walletString = localStorage.getItem('masterWallet')
+    }
+    return JSON.parse(walletString!)
+}
+
+export async function createPOAP(poapHash: string) {
+    const masterWallet = getMasterWallet();
+    let subWallet: ethers.utils.HDNode = ethers.utils.HDNode.fromMnemonic(masterWallet.mnemonic.phrase);
+
+    // TODO: check if subWallet has any content or is empty => Dune API ?
+
+    // Get POAP API Secret
+    const secretFetchOptions = {
+        method: 'GET',
+        url: 'https://api.poap.tech/actions/claim-qr?qr_hash=' + poapHash,
+        headers: {
+            accept: 'application/json',
+            'X-API-Key': POAP_API_KEY,
+            authorization: 'Bearer ' + POAP_API_TOKEN
+        }
+    };
+
+    let secretString: string = '';
+
+    await axios
+        .request(secretFetchOptions)
+        .then(function (respose) {
+            secretString = respose.data.secret
+            console.log(secretString);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+
+
+    // Mint POAP
+    const poapMintOptions = {
+        method: 'POST',
+        url: 'https://api.poap.tech/actions/claim-qr',
+        headers: {
+            accept: 'application/json',
+            'content-type': 'application/json',
+            'X-API-Key': 'b',
+            authorization: 'Bearer ' + POAP_API_TOKEN
+        },
+        data: {
+            sendEmail: true,
+            address: subWallet.address,
+            qr_hash: poapHash,
+            secret: secretString
+        }
+    };
+
+    let poap;
+
+    await axios
+        .request(poapMintOptions)
+        .then(function (response) {
+            poap = response.data;
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
 }
