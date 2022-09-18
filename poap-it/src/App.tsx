528da087c63b9123471e67871a6dbc61b31dfafd
marcsinner <marc.sinner@t-online.de>
2022-09-18 06:34:37 +0200

    add show POAPs

--- poap-it/src/App.tsx
+++ poap-it/src/App.tsx
@@ -1,16 +1,43 @@
-import { Button, Input  } from '@chakra-ui/react';
-import React, { useState } from 'react';
-import './App.css';
-import { getMasterWallet , createPOAP} from "./services/wallet.service";
-import {MintPoap} from './components/mintPoap';
-
+import { Button, Input } from "@chakra-ui/react";
+import React, { useState } from "react";
+import "./App.css";
+import { getMasterWallet, createPOAP } from "./services/wallet.service";
+import { MintPoap } from "./components/mintPoap";
+import { Grid, GridItem } from "@chakra-ui/react";
+import { SeePoap } from './components/seePoap';
 
 function App() {
-    return (
-        <div className="App">
-            <MintPoap poapID='' masterWalletAddress={localStorage.getItem('masterWalletAddress') ?? ""}/>
-            
-            {/* <Button onClick={() => {
+  return (
+    <div className="App">
+      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+        <GridItem w="100%" h="100%" bg="blue.500">
+  
+          <MintPoap
+            poapID=""
+            masterWalletAddress={
+              localStorage.getItem("masterWalletAddress") ?? ""
+            }
+            masterWalletMemonic={
+              localStorage.getItem("masterWalletMnemonic") ?? "No Master Wallet"
+            }
+          />
+        </GridItem>
+        <GridItem w="100%" h="100%" bg="blue.500">
+      
+          <SeePoap
+          
+            masterWalletAddress={
+              localStorage.getItem("masterWalletAddress") ?? ""
+            }
+            masterWalletMemonic={
+              localStorage.getItem("masterWalletMnemonic") ?? "No Master Wallet"
+            }
+        poaps={[]}
+          />
+        </GridItem>
+      </Grid>
+
+      {/* <Button onClick={() => {
                 console.log(getMasterWallet())
             }} colorScheme='teal' variant='outline'>
                 GOOOOOOO
@@ -20,9 +47,8 @@ function App() {
             }} colorScheme='teal' variant='outline'>
                 get poap
             </Button> */}
-
-        </div>
-    );
+    </div>
+  );
 }
 
 export default App;
--- poap-it/src/services/wallet.service.ts
+++ poap-it/src/services/wallet.service.ts
@@ -1,10 +1,12 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { ethers } from 'ethers';
+import { Poap } from '../components/seePoap';
 
 const POAP_API_KEY = '';
 const POAP_API_TOKEN = '';
 const DUNE_API_QUERY_ID = ''
 const DUNE_API_KEY = ''
+
 
 export function InitWallet() {
     let masterWallet = ethers.Wallet.createRandom();
@@ -13,13 +15,146 @@ export function InitWallet() {
     localStorage.setItem('masterWalletMnemonic', String(masterWallet.mnemonic.phrase));
 }
 
+
+
+export async function getPOAPInfo(poap_id:number): Promise<string> {
+
+    var config = {
+        method: 'get',
+        url: 'https://api.poap.tech/token/' + poap_id,
+        headers: {
+            'X-API-Key': POAP_API_KEY,
+        },
+    };
+
+    let result = await axios(config)
+        .then(function (response) {
+
+            var result = JSON.stringify(response.data)
+            console.log(result)
+            return result
+        })
+        .catch(function (error) {
+            console.log(error);
+            return "{}"
+        });
+
+        return result
+}
+
+export async function getAllPOAPs() :  Promise<Poap[]>{
+
+    // TODO get poaps
+
+    let collection = await parsePOAPCollection()
+    let asset_list: Poap[] = []
+
+    console.log(collection)
+
+    collection.forEach(async (e)=>{
+
+    let poap_info = JSON.parse(await getPOAPInfo(e)) as Poap
+    console.log()
+    asset_list.push(poap_info)
+        
+    })
+
+    console.log(asset_list)
+
+    return asset_list
+
+}
+
 export function getMasterWallet(): ethers.Wallet {
     let walletString = localStorage.getItem('masterWalletAddress')
     if (walletString === null || walletString === '') {
         InitWallet();
         walletString = localStorage.getItem('masterWalletAddress')
     }
-    return JSON.parse(walletString!)
+    console.log(walletString)
+    return JSON.parse(JSON.stringify(walletString!))
+}
+
+export async function parsePOAPCollection() {
+    let isEmptySubwallet = false;
+    let mnemonic_phrase = localStorage.getItem('masterWalletMnemonic')
+    let masterWallet: ethers.utils.HDNode = ethers.utils.HDNode.fromMnemonic(String(mnemonic_phrase));
+    let index = 0;
+    let subWallet = masterWallet.derivePath(`m/44'/60'/${index}'/0/0`);
+    let i = 10;
+
+    let mapWalletPOAP = new Map();//[['key1', 'value1'], ['key2', 'value2'], ['keyn', 'valuen']]);
+
+    while (i != 0 && !isEmptySubwallet) {
+        i--;
+        subWallet = masterWallet.derivePath(`m/44'/60'/${index}'/0/0`);
+        index++;
+        // Execute Dune Query
+        console.log("url posted to:", 'https://api.dune.com/api/v1/query/' + DUNE_API_QUERY_ID + '/execute')
+        const duneExecutionsOptions = {
+            method: 'POST',
+            url: 'https://api.dune.com/api/v1/query/' + DUNE_API_QUERY_ID + '/execute?Address=' + subWallet.address,
+            headers: {
+                // 'accept': 'application/json',
+                'x-dune-api-key': DUNE_API_KEY,
+                // 'Access-Control-Allow-Origin': '*',
+                // 'Access-Control-Allow-Credentials':true
+                // authorization: 'Bearer ' + POAP_API_TOKEN
+            },
+            data: {
+                "query_parameters": {
+                    "Address": subWallet.address
+                }
+            }
+
+        };
+        console.log(subWallet.address)
+        let executionID: string = '';
+        await axios
+            //@ts-ignore
+            .request(duneExecutionsOptions)
+            .then(function (respose) {
+                executionID = respose.data.execution_id;
+                console.log(executionID);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+
+        // Query Result
+        const duneResultOptions = {
+            method: 'GET',
+            url: 'https://api.dune.com/api/v1/execution/' + executionID + '/results',
+            headers: {
+                // 'accept': 'application/json',
+                'x-dune-api-key': DUNE_API_KEY,
+                // 'authorization': 'Bearer ' + POAP_API_TOKEN,
+                // 'Access-Control-Allow-Origin': '*'
+            }
+        };
+        await new Promise(f => setTimeout(f, 5000));
+
+        await axios
+            //@ts-ignore
+
+            .request(duneResultOptions)
+            .then(function (response) {
+                console.log(response.data)
+                let duneQueryResult = response.data;
+                if (duneQueryResult.result) {
+                    if (duneQueryResult.result.rows.length === 0) isEmptySubwallet = true;
+                    else mapWalletPOAP.set(subWallet.address, duneQueryResult.result.rows[0].tokenId);
+
+                }
+                
+                
+                console.log(mapWalletPOAP);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    }
+    return mapWalletPOAP
 }
 
 export async function createPOAP(poapHash: string) {
